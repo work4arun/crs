1297d9c2442426e11b8b7f6ee8b6e68b393cd2e9
work4arun <tx2arun@gmail.com>
2026-02-10 23:25:01 +0530

    fix: ensure db password is set

--- deploy/setup_db.sh
+++ deploy/setup_db.sh
@@ -24,6 +24,7 @@ DB_PASS="growthcard123"
 echo "Creating database user '$DB_USER' and database '$DB_NAME'..."
 
 sudo -u postgres psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';" || echo "User might already exist"
+sudo -u postgres psql -c "ALTER USER $DB_USER WITH PASSWORD '$DB_PASS';"
 sudo -u postgres psql -c "CREATE DATABASE $DB_NAME OWNER $DB_USER;" || echo "Database might already exist"
 sudo -u postgres psql -c "ALTER USER $DB_USER CREATEDB;"
 
